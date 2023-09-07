871f8ea5a37b00737890b20601aef87052e0cb6d
MVIvien <v.mavel@bopen.eu>
2023-09-07 09:22:08 +0200

    add generation of plot1 to server.py

--- src/server.py
+++ src/server.py
@@ -1,9 +1,13 @@
 import geopandas as gpd
 import xarray as xr
+import json
 import os
 
 from flask import Flask, request, send_file, Response, jsonify
 from flask_cors import CORS
+
+import plotly.express as px
+import plotly.io as io
 
 app = Flask(__name__)
 CORS(app)
@@ -32,6 +36,24 @@ def generate_geojson():
     gdf.to_file("../public/reduced_data.json", driver="GeoJSON")
 
     return send_file("../public/reduced_data.json", mimetype="application/json")
+
+
+@app.route("/plot1")
+def plot1():
+    rcp = request.args.get("rcp", type=str)
+
+    filename = os.path.join(
+            os.path.dirname(__file__),
+            f"../public/01_mean_temperature_projections-seasonal-{rcp}-rca4-ec_earth-r12i1p1-layer-nuts_0-latitude-v0.2.nc"
+        )
+    data = xr.open_dataarray(filename)
+    sel = data.sel(nuts="FR") - 273.15
+    fig = px.line(sel.data)
+
+    io.write_json(fig, "../public/plot1.json")
+
+    return send_file("../public/plot1.json", mimetype="application/json")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
