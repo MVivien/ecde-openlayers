66a4f9754f98f47abddc00ba7ff7a0827b180105
MVIvien <v.mavel@bopen.eu>
2023-09-14 11:53:56 +0200

    take in region as a new variable

--- src/Chart.jsx
+++ src/Chart.jsx
@@ -1,24 +1,24 @@
 import Plotly from 'plotly.js-dist-min';
 import { useEffect, useState } from 'react';
 
-function Chart({ rcp }) {
+function Chart({ rcp, region } ) {
   const [plotData, setPlotData] = useState(null);
 
   useEffect(() => {
     async function loadPlot() {
-      const plot = await fetch(`http://localhost:5000/plot1?rcp=${rcp}`);
+      const plot = await fetch(`http://localhost:5000/plot1?rcp=${rcp}&region=${region}`);
       const json = await plot.json();
       setPlotData(json);
     }
     loadPlot();
-  }, [rcp]);
+  }, [rcp, region]);
 
   useEffect(() => {
     if (!plotData) {
       return;
     }
     Plotly.newPlot('plotly', plotData.data, plotData.layout);
-  }, [rcp, plotData]);
+  }, [rcp, region, plotData]);
 
   return <section className="plotly-chart" id="plotly" />;
 }
