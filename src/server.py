57b7d8cf835e0001eb0c4f2bfcc7d6d3d68061c0
MVIvien <v.mavel@bopen.eu>
2023-09-15 18:00:35 +0200

    handle different layers in geojson

--- src/server.py
+++ src/server.py
@@ -4,6 +4,7 @@ import os
 
 from flask import Flask, request, send_file, Response, jsonify
 from flask_cors import CORS
+import fsspec
 
 import plotly.express as px
 import plotly.io as io
@@ -12,23 +13,28 @@ app = Flask(__name__)
 CORS(app)
 
 
-@app.route("/geojson")
-def generate_geojson():
+@app.route("/geojson/<path:layer>")
+def generate_geojson(layer):
     rcp = request.args.get("rcp", type=str)
 
-    filename = os.path.join(
-            os.path.dirname(__file__),
-            f"../public/01_mean_temperature_projections-seasonal-{rcp}-rca4-ec_earth-r12i1p1-layer-nuts_0-latitude-v0.2.nc"
-        )
+    url = f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-projections-yearly-layer-{layer}-latitude-v0.3-30yrs_average.nc"
 
-    data = xr.open_dataarray(filename).mean(dim="time") - 273.15
+    with fsspec.open(f"filecache::{url}", filecache={"same_names": True}) as f:
+        data = xr.open_dataarray(f.name)
+
+    data = data.sel(scenario=rcp, avg_period='2011-01-01')
     df = data.to_dataframe().reset_index()
     df = df.rename(columns={"nuts": "NUTS_ID", data.name: "value"})
 
+    level = {
+        'nuts_0': 'LEVL_0',
+        'nuts_1': 'LEVL_1',
+        'nuts_2': 'LEVL_2',
+    }[layer]
     geodataframe = gpd.read_file(
         os.path.join(
             os.path.dirname(__file__),
-            '../public/NUTS_RG_60M_2021_4326_LEVL_0.geojson'
+            f'../public/NUTS_RG_60M_2021_4326_{level}.geojson'
         )
     )
     gdf = geodataframe.merge(df, on='NUTS_ID')
