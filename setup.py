1ccd41841f4c6ac5d39b5bf751d165d7e663edf2
Luca Fabbri <l.fabbri@bopen.eu>
2023-09-19 15:32:44 +0200

    Fix env python

--- setup.py
+++ setup.py
@@ -4,5 +4,12 @@ setup(
     name="ecde-openlayer-app",
     version="0.1.0",
     packages=["src"],
-    install_requires=["cdsapi", "xarray", "netcdf4", "geopandas", "flask"],
+    install_requires=[
+        "cdsapi",
+        "xarray",
+        "netcdf4",
+        "geopandas",
+        "flask",
+        "flask_cors",
+    ],
 )
