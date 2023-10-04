928614ae8f4766756a03d08252d0fe446b9b17b1
Marco Cucchi <m.cucchi@bopen.eu>
2023-10-04 13:46:59 +0200

    historical_anomalies plot

--- src/app/plots.py
+++ src/app/plots.py
@@ -1,10 +1,45 @@
 import plotly.graph_objects as go
 import xarray as xr
 
-def historical_anomalies(data: xr.DataArray):
+COLORS = {"light_gray": "rgb(182, 182, 182)", "lighter_gray": "rgb(236, 236, 236)"}
+LAYOUT = go.Layout(
+    paper_bgcolor="rgb(255,255,255)",
+    plot_bgcolor="rgb(255,255,255)",
+    xaxis=go.XAxis(
+        title={"text": ""},
+        color=COLORS["light_gray"],
+        gridcolor=COLORS["lighter_gray"],
+    ),
+    yaxis=go.YAxis(
+        color=COLORS["light_gray"],
+        gridcolor=COLORS["lighter_gray"],
+        side="right",
+    ),
+    height=300,
+    margin={"l": 25, "r": 55, "b": 30, "t": 30},
+    hovermode="x",
+)
+
+
+def historical_anomalies(
+    data: xr.DataArray,
+    positive_color: str = "rgb(0.77,0.23,0.23)",
+    negative_color: str = "rgb(0.19,0.49,0.72)",
+    units: str = "",
+):
     df = data.to_pandas()
-    fig = go.Figure()
+    bar_color = [
+        positive_color if value >= 0 else negative_color for value in df.values
+    ]
+    fig = go.Figure(layout=LAYOUT)
     fig = fig.add_trace(
-        go.Bar(x=df.index, y=df.values, name=data.name)
+        go.Bar(
+            x=df.index,
+            y=df.values,
+            name=data.name,
+            marker={"color": bar_color},
+            hovertemplate=f"%{{x}}, %{{y:.1f}} {units}<extra></extra>",  # noqa: F541
+        )
     )
+    fig.update_layout(yaxis_title=f"Anomaly ({units})")
     return fig
--- src/app/server.py
+++ src/app/server.py
@@ -22,11 +22,15 @@ def generate_geojson(
     temporal_aggregation: str = fastapi.Query(..., alias="temporalAggregation"),
 ) -> fastapi.responses.StreamingResponse:
     if horizon == "1981-01-01":
-      url = f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-historical" \
+        url = (
+            f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-historical"
             f"-{temporal_aggregation}-layer-{layer}-latitude-1959-2022-v0.2-30yrs_average.nc"
+        )
     else:
-      url = f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-projections" \
+        url = (
+            f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-projections"
             f"-{temporal_aggregation}-layer-{layer}-latitude-v0.3-30yrs_average.nc"
+        )
 
     with fsspec.open(f"filecache::{url}", filecache={"same_names": True}) as f:
         data = xr.open_dataarray(f.name)
@@ -38,16 +42,20 @@ def generate_geojson(
     df = df.rename(columns={"nuts": "NUTS_ID", data.name: "value"})
 
     level = {
-        'nuts_0': 'LEVL_0',
-        'nuts_1': 'LEVL_1',
-        'nuts_2': 'LEVL_2',
+        "nuts_0": "LEVL_0",
+        "nuts_1": "LEVL_1",
+        "nuts_2": "LEVL_2",
     }[layer]
-    geodataframe = gpd.read_file(os.path.join(DIR, f'../../public/NUTS_RG_60M_2021_4326_{level}.geojson'))
-    gdf = geodataframe.merge(df, on='NUTS_ID')
-    gdf = gdf.to_crs('epsg:3035')
+    geodataframe = gpd.read_file(
+        os.path.join(DIR, f"../../public/NUTS_RG_60M_2021_4326_{level}.geojson")
+    )
+    gdf = geodataframe.merge(df, on="NUTS_ID")
+    gdf = gdf.to_crs("epsg:3035")
     gdf.to_file(os.path.join(DIR, "../../public/reduced_data.json"), driver="GeoJSON")
 
-    return fastapi.responses.FileResponse(os.path.join(DIR, "../../public/reduced_data.json"))
+    return fastapi.responses.FileResponse(
+        os.path.join(DIR, "../../public/reduced_data.json")
+    )
 
 
 @app.get("/plots/historical_anomalies")
@@ -56,21 +64,28 @@ def historical_anomalies(
     selected_layer: str = fastapi.Query(..., alias="selectedLayer"),
     temporal_aggregation: str = fastapi.Query(..., alias="temporalAggregation"),
 ):
-    url = f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-historical" \
-          f"-{temporal_aggregation}-layer-nuts_{selected_layer}-latitude-1959-2022-v0.2-anomalies.nc"
+    url = (
+        f"https://ecde-data.copernicus-climate.eu/05_tropical_nights/plots/05_tropical_nights-historical"
+        f"-{temporal_aggregation}-layer-nuts_{selected_layer}-latitude-1959-2022-v0.2-anomalies.nc"
+    )
     print(url)
     with fsspec.open(f"filecache::{url}", filecache={"same_names": True}) as f:
         data = xr.open_dataarray(f.name)
     sel = data.sel(nuts=region)
-    fig = plots.historical_anomalies(sel)
+    fig = plots.historical_anomalies(sel, units="days")
 
-    io.write_json(fig, os.path.join(DIR, f"../../public/historical_anomalies_{selected_layer}.json"))
+    io.write_json(
+        fig,
+        os.path.join(DIR, f"../../public/historical_anomalies_{selected_layer}.json"),
+    )
 
-    return fastapi.responses.FileResponse(os.path.join(DIR, f"../../public/historical_anomalies_{selected_layer}.json"))
+    return fastapi.responses.FileResponse(
+        os.path.join(DIR, f"../../public/historical_anomalies_{selected_layer}.json")
+    )
 
 
 origins = [
-   "http://localhost:5173",
+    "http://localhost:5173",
 ]
 app.add_middleware(
     fastapi.middleware.cors.CORSMiddleware,
